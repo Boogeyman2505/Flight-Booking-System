3875e449771168e09d1ad1cd45e3583b848feb4e
Urkudkar <ketan.a.urkudkar@capgemini.com>
2023-11-30 19:15:00 +0530

    commit

---
+++ Frontend/src/app/flight.ts
@@ -0,0 +1,21 @@
+// export class Flight {
+//     constructor(
+//    public  flightNo: String,
+// 	  public  flightName: String,
+// 	  public  flightFrom: String,
+// 	  public  flightTo: String,
+// 	  public  fare: number,
+// 	  public  seats: number,
+// 	  public  time: String
+//     ) {}
+// }
+export class Flight {
+    flightNo!: String;
+    flightName!: String;
+    flightFrom!: String;
+    flightTo!: String;
+    fare!: number;
+    seats!: number;
+    time!: String;
+    
+}
